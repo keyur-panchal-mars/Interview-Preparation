121e5aee1eca0013ba57fc5c69f8af70dce04ccb
Keyur Panchal <panchalkeyur1999@gmail.com>
2024-11-29 13:56:02 +0530

    updated version updates

--- Javascript/Practicals/array.js
+++ Javascript/Practicals/array.js
@@ -134,5 +134,19 @@ console.log(isPalindromeArray([1, 2, 3, 2, 1])); // Output: true
 console.log(isPalindromeArray([1, 2, 3, 4]));    // Output: false
 
 // ---------------------------------------------------------------------------
+// Return Single Number
 
+function singleNumber(nums) {
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length; i += 2) {
+        if (nums[i] !== nums[i + 1]) {
+            return nums[i];
+        }
+    }
+    return nums[nums.length - 1];
+};
+
+console.log(singleNumber([1,2,3,4,1,2,3])) // 4
+
+// ---------------------------------------------------------------------------
 
